7ab1c7d1c343d44b3c22ecbe187a07719cdc44a3
from20020516 <lofi.personal@gmail.com>
2022-04-05 20:43:00 +0900

    `$ amplify push`

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "chatapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
